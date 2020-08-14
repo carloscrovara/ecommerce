14607df17eb334063ced26b820f1355b87fbbdf1
carloscrovara <carloscrovara@gmail.com>
2020-08-14 17:28:20 -0300

    Version 1.9 - Modificaciones menores en Item e ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,14 +6,14 @@ export default function ItemList({ products }) {
             <div className="album py-5 bg-light">
                 <div className="container">
                     <div className="text-center row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                        {products.map((p, index) => 
-                            <div className="col" style={{ paddingBottom: '20px'}} key={index}> 
+                        {products.map((p, idx) => 
+                            <div className="col" style={{ paddingBottom: '20px'}} key={p.id}> 
                                 <div className="card shadow-sm">
                                     <img src={p.img} className="card-img-top" style={{ height: '320px'}} alt="product"></img>
                                     <div className="card-body">
                                         <h2 className="card-title" id={p.id}>{p.name}</h2>
                                         <div className="text-center"> 
-                                            <h3 className="card-title pricing-card-title" style={{ color: '#ff7043'}}>{p.price}</h3>
+                                            <h3 className="card-title pricing-card-title" style={{ color: '#ff7043'}}>$ {p.price}</h3>
                                             <a href="#product" 
                                                 className="btn btn-success" 
                                                 onClick={function masInfo() {console.log("Va a página con el detalle del producto " + p.name)}}
