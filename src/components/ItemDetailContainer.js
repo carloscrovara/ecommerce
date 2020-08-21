4da98cf29fcf355f56c47a39792c77d61d083dc4
carloscrovara <carloscrovara@gmail.com>
2020-08-21 10:16:41 -0300

    Version 1.14

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -51,7 +51,7 @@ function ItemDetailContainer({ valueIdItem }) {
             setLoading(false);
             console.log(product)
     });
-    }, []);
+    }, [product, valueIdItem]);
     
     return (
         <>
