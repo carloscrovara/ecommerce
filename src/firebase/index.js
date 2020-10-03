cec10d7cff58db7eb7cfc7e0b981a9e033dc75dd
carloscrovara <carloscrovara@gmail.com>
2020-10-02 21:17:00 -0300

    Version 1.51 - Cambios menores

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBiEzTzYWbTsJiPwI83tiysInKL10fIFUw",
-    authDomain: "ecommerce-carlos.firebaseapp.com",
-    databaseURL: "https://ecommerce-carlos.firebaseio.com",
-    projectId: "ecommerce-carlos",
-    storageBucket: "ecommerce-carlos.appspot.com",
-    messagingSenderId: "347728904453",
-    appId: "1:347728904453:web:d7f699561225f6b60fb76c",
+    apiKey: "tuApiKey",
+    authDomain: "tuauthDomain",
+    databaseURL: "tudatabaseURL",
+    projectId: "tuProjectId",
+    storageBucket: "tuStorageBucket",
+    messagingSenderId: "tumessagingSenderId",
+    appId: "tuAppId",
 });
 
 export function getFirebase() {
